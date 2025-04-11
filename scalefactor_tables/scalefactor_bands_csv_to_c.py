733c6c25f0dfe3ddec9fc0f98085522a420e1b51
alvisngan <alvischngan@gmail.com>
2025-04-11 00:03:31 -0400

    fix: missing [] during initiation

--- scalefactor_tables/scalefactor_bands_csv_to_c.py
+++ scalefactor_tables/scalefactor_bands_csv_to_c.py
@@ -30,26 +30,25 @@ file_text_str = ''
 
 for freq in freq_list:
     for block in block_types:
-        print(freq)
         filename = "scalefactor_bands_" + block + "_block_" + freq + ".csv"
 
         df = pd.read_csv(filename, dtype = int)
 
         bitsize_str = ("uint8_t " + s_prefix_str  + "scf_" + freq + "_" +
-                          block + "_ibitsize = {")
+                          block + "_ibitsize[] = {")
         for num in df.loc[:, "bitsize"].astype(str).values:
             bitsize_str += num + ", "
         bitsize_str = static_str + const_str + bitsize_str[:-2] + "};"
 
         idx_start_str = ("uint16_t " + s_prefix_str  + "scf_" + freq + "_" +
-                          block + "_idx_start = {")
+                          block + "_idx_start[] = {")
         for num in df.loc[:, "idx_start"].astype(str).values:
             idx_start_str += num + ", "
         idx_start_str = static_str + const_str + idx_start_str[:-2] + "};"
 
 
         idx_end_str = ("uint16_t " + s_prefix_str  + "scf_" + freq + "_" +
-                          block + "_idx_end = {")
+                          block + "_idx_end[] = {")
         for num in df.loc[:, "idx_end"].astype(str).values:
             idx_end_str += num + ", "
         idx_end_str = static_str + const_str + idx_end_str[:-2] + "};"
--- scalefactor_tables/scalefactor_tables.c
+++ scalefactor_tables/scalefactor_tables.c
@@ -1,24 +1,24 @@
-static const uint8_t s_scf_32000hz_long_ibitsize = {4, 4, 4, 4, 4, 4, 6, 6, 8, 10, 12, 16, 20, 24, 30, 38, 46, 56, 68, 84, 102};
-static const uint16_t s_scf_32000hz_long_idx_start = {0, 4, 8, 12, 16, 20, 24, 30, 36, 44, 54, 66, 82, 102, 126, 156, 194, 240, 296, 364, 448};
-static const uint16_t s_scf_32000hz_long_idx_end = {3, 7, 11, 15, 19, 23, 29, 35, 43, 53, 65, 81, 101, 125, 155, 193, 239, 295, 363, 447, 549};
+static const uint8_t s_scf_32000hz_long_ibitsize[] = {4, 4, 4, 4, 4, 4, 6, 6, 8, 10, 12, 16, 20, 24, 30, 38, 46, 56, 68, 84, 102};
+static const uint16_t s_scf_32000hz_long_idx_start[] = {0, 4, 8, 12, 16, 20, 24, 30, 36, 44, 54, 66, 82, 102, 126, 156, 194, 240, 296, 364, 448};
+static const uint16_t s_scf_32000hz_long_idx_end[] = {3, 7, 11, 15, 19, 23, 29, 35, 43, 53, 65, 81, 101, 125, 155, 193, 239, 295, 363, 447, 549};
 
-static const uint8_t s_scf_32000hz_short_ibitsize = {4, 4, 4, 4, 6, 8, 12, 16, 20, 26, 34, 42};
-static const uint16_t s_scf_32000hz_short_idx_start = {0, 4, 8, 12, 16, 22, 30, 42, 58, 78, 104, 138};
-static const uint16_t s_scf_32000hz_short_idx_end = {3, 7, 11, 15, 21, 29, 41, 57, 77, 103, 137, 179};
+static const uint8_t s_scf_32000hz_short_ibitsize[] = {4, 4, 4, 4, 6, 8, 12, 16, 20, 26, 34, 42};
+static const uint16_t s_scf_32000hz_short_idx_start[] = {0, 4, 8, 12, 16, 22, 30, 42, 58, 78, 104, 138};
+static const uint16_t s_scf_32000hz_short_idx_end[] = {3, 7, 11, 15, 21, 29, 41, 57, 77, 103, 137, 179};
 
-static const uint8_t s_scf_44100hz_long_ibitsize = {4, 4, 4, 4, 4, 4, 6, 6, 8, 8, 10, 12, 16, 20, 24, 28, 34, 42, 50, 54, 76};
-static const uint16_t s_scf_44100hz_long_idx_start = {0, 4, 8, 12, 16, 20, 24, 30, 36, 44, 52, 62, 74, 90, 110, 134, 162, 196, 238, 288, 342};
-static const uint16_t s_scf_44100hz_long_idx_end = {3, 7, 11, 15, 19, 23, 29, 35, 43, 51, 61, 73, 89, 109, 133, 161, 195, 237, 287, 341, 417};
+static const uint8_t s_scf_44100hz_long_ibitsize[] = {4, 4, 4, 4, 4, 4, 6, 6, 8, 8, 10, 12, 16, 20, 24, 28, 34, 42, 50, 54, 76};
+static const uint16_t s_scf_44100hz_long_idx_start[] = {0, 4, 8, 12, 16, 20, 24, 30, 36, 44, 52, 62, 74, 90, 110, 134, 162, 196, 238, 288, 342};
+static const uint16_t s_scf_44100hz_long_idx_end[] = {3, 7, 11, 15, 19, 23, 29, 35, 43, 51, 61, 73, 89, 109, 133, 161, 195, 237, 287, 341, 417};
 
-static const uint8_t s_scf_44100hz_short_ibitsize = {4, 4, 4, 4, 6, 8, 10, 12, 14, 18, 22, 30};
-static const uint16_t s_scf_44100hz_short_idx_start = {0, 4, 8, 12, 16, 22, 30, 40, 52, 66, 84, 106};
-static const uint16_t s_scf_44100hz_short_idx_end = {3, 7, 11, 15, 21, 29, 39, 51, 65, 83, 105, 135};
+static const uint8_t s_scf_44100hz_short_ibitsize[] = {4, 4, 4, 4, 6, 8, 10, 12, 14, 18, 22, 30};
+static const uint16_t s_scf_44100hz_short_idx_start[] = {0, 4, 8, 12, 16, 22, 30, 40, 52, 66, 84, 106};
+static const uint16_t s_scf_44100hz_short_idx_end[] = {3, 7, 11, 15, 21, 29, 39, 51, 65, 83, 105, 135};
 
-static const uint8_t s_scf_48000hz_long_ibitsize = {4, 4, 4, 4, 4, 4, 6, 6, 6, 8, 10, 12, 16, 18, 22, 28, 34, 40, 46, 54, 54};
-static const uint16_t s_scf_48000hz_long_idx_start = {0, 4, 8, 12, 16, 20, 24, 30, 36, 42, 50, 60, 72, 88, 106, 128, 156, 190, 230, 276, 330};
-static const uint16_t s_scf_48000hz_long_idx_end = {3, 7, 11, 15, 19, 23, 29, 35, 41, 49, 59, 71, 87, 105, 127, 155, 189, 229, 275, 329, 383};
+static const uint8_t s_scf_48000hz_long_ibitsize[] = {4, 4, 4, 4, 4, 4, 6, 6, 6, 8, 10, 12, 16, 18, 22, 28, 34, 40, 46, 54, 54};
+static const uint16_t s_scf_48000hz_long_idx_start[] = {0, 4, 8, 12, 16, 20, 24, 30, 36, 42, 50, 60, 72, 88, 106, 128, 156, 190, 230, 276, 330};
+static const uint16_t s_scf_48000hz_long_idx_end[] = {3, 7, 11, 15, 19, 23, 29, 35, 41, 49, 59, 71, 87, 105, 127, 155, 189, 229, 275, 329, 383};
 
-static const uint8_t s_scf_48000hz_short_ibitsize = {4, 4, 4, 4, 6, 6, 10, 12, 14, 16, 20, 26};
-static const uint16_t s_scf_48000hz_short_idx_start = {0, 4, 8, 12, 16, 22, 28, 38, 50, 64, 80, 100};
-static const uint16_t s_scf_48000hz_short_idx_end = {3, 7, 11, 15, 21, 27, 37, 49, 63, 79, 99, 125};
+static const uint8_t s_scf_48000hz_short_ibitsize[] = {4, 4, 4, 4, 6, 6, 10, 12, 14, 16, 20, 26};
+static const uint16_t s_scf_48000hz_short_idx_start[] = {0, 4, 8, 12, 16, 22, 28, 38, 50, 64, 80, 100};
+static const uint16_t s_scf_48000hz_short_idx_end[] = {3, 7, 11, 15, 21, 27, 37, 49, 63, 79, 99, 125};
 
